372e30454bda319a94142a4c1ad5558528f92299
Mohamed <askytrix@gmail.com>
2023-08-11 01:06:54 +0300

    Added More Logic to Backend

---
+++ api/models/User.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    distance: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    photos: {
+      type: [String],
+    },
+    desc: {
+      type: String,
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+    },
+    rooms: {
+      type: [String],
+    },
+    cheapestPrice: {
+      type: Number,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,38 @@
 import express from "express";
 
-const router = express.Router();
+import {
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
-router.get("/", (req, res) => {
-  res.send("Users End-Point");
+const router = express.Router();
+/*
+router.get("/checkauth", verifyToken, (req, res, next) => {
+  res.send(req.user);
 });
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+  res.send("Role: User, already logged in.");
+});
+
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+  res.send("Role: User, already logged in.");
+});
+*/
+
+// Update
+router.put("/:id", verifyUser, updateUser);
+
+// Delete
+router.delete("/:id", verifyUser, deleteUser);
+
+// Get
+router.get("/:id", verifyUser, getUser);
+
+// Get All
+router.get("/", verifyAdmin, getAllUsers);
 
 export default router;
