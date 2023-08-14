b48e59006e87258cc2fbc0f36bbf9ea4cbd8b9a5
AntonNageh <92064286+AntonNageh@users.noreply.github.com>
2023-08-14 20:02:20 +0300

    AddedaBluryBackgn

--- frontend/booking-lab/src/Pages/Signup/Signup.jsx
+++ frontend/booking-lab/src/Pages/Signup/Signup.jsx
@@ -47,7 +47,7 @@ const Signup = () => {
   const successMessage = () => {
     return (
         submitted?
-        <div className='z-50 bg-white h-screen -mt-[10rem] absolute w-screen'>
+        <div className='z-10 backdrop-blur-md w-full h-screen -mt-[40rem] absolute '>
         <Success/>
         </div>
         : null
@@ -71,54 +71,57 @@ const Signup = () => {
   return (
     <div className='text-black flex flex-col text-center w-11/12 ml-[1em] mt-[5%] Gradient-Bg py-10'>
       <h1 className='text-3xl font-bold'>Please <br/> Signup below</h1>
-      <div className="form mt-5">
+      <div className="form mt-5 ml-5">
         {/* Calling to the methods */}
         <div className="messages text-red-500 text-2xl font-bold">
           {errorMessage()}
         </div>
-        <div className="messages text-green-600 text-2xl font-bold">
-          {successMessage()}
-          </div>
         <form className='mt-5 w-9/12 text-2xl font-bold justify-items-center ml-[2.5rem]'>
           {/* Labels and inputs for form data */}
           <label className="label block mb-5">Name</label>
           <input 
-          onChange={handleName} 
-          className="input mb-3"
+          className="input mb-3 placeholder:text-2xl"
           required={true}
           value={name}
           name="name" 
           placeholder='Ex: John Doe'
           autoFocus
+          min={5}
+          onChange={handleName} 
           type="text" /><br/>
 
           <label className="label block mb-5">Email</label>
           <input 
-          onChange={handleEmail} 
           placeholder='Ex: JohnDoe@example.com'
           required
           name="email"
-          className="input mb-3"
+          className="input mb-3 placeholder:text-base"
           value={email} 
+          onChange={handleEmail} 
           type="email" /><br/>
 
           <label className="label block mb-5">Password</label>
           <input 
-          onChange={handlePassword} 
-          className="input"
-          placeholder='Enter your password.'
+          className="input placeholder:text-base"
+          min={8}
+          max={12}
+          placeholder='Minimum is 8 numbers, symbols or strings.'
           value={password}
           name="password" 
           required
+          onChange={handlePassword} 
           type="password" /><br/>
           <button 
-          onClick={handleSubmit} 
           className="BoxShadow mt-9 p-5 w-11/12 rounded-full InitialBg" 
+          onClick={handleSubmit} 
           type="submit">
           Submit
           </button>
         </form>
       </div>
+        <div className="messages text-green-600 text-2xl font-bold">
+          {successMessage()}
+          </div>
     </div>
   )
 }
--- frontend/booking-lab/src/Pages/Signup/Success/Success.jsx
+++ frontend/booking-lab/src/Pages/Signup/Success/Success.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Success = () => {
   return (
-    <div className='mt-[20rem] w-9/12 text-center ml-10'>
-       <h1 className='text-green-500 text-5xl mb-10'>Successfully Signed up! </h1> 
+    <div className=''>
+      <div className='mt-[10em] h-full w-9/12 text-center ml-10'>
+        <h1 className='text-green-500 text-5xl mb-10'>Successfully Signed up! </h1> 
         <h1 className='text-2xl text-black'>you'll be redirected to the main page..</h1>
+      </div>
  
     </div>
   )
